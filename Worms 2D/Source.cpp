ed3ce2cd0c917727193694d4b20058fb9b60cc98
Cherro <kaliszewski.dominik@gmail.com>
2018-10-08 15:41:47 +0200

    Add main endpoint of project with simple SFML Window test and events

---
+++ Worms 2D/Source.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(1280, 1024), "Worms 2D");
+
+	while(window.isOpen())
+	{
+		sf::Event ev;
+		while(window.pollEvent(ev))
+		{
+			if(ev.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+	}
+
+	return 0;
+}
