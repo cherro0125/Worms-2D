7b9a71010a126cddc028292de9d8b5eae280b19e
Cherro <kaliszewski.dominik@gmail.com>
2018-10-08 22:08:20 +0200

    Change primitives shape in worm class to sprite with basic textures

--- Worms 2D/Worm.cpp
+++ Worms 2D/Worm.cpp
@@ -1,16 +1,22 @@
 ﻿#include "Worm.h"
 #include <string>
-
+#include <iostream>
 
 
 Worm::Worm(float width, float height) : windowWidth(width), windowHeight(height)
 {
 	this->posX = width / 2;
 	this->posY = height / 2;
-	this->shape.setPosition(this->posX, this->posY);
-	this->shape.setRadius(wormRadius);
-	this->shape.setFillColor(sf::Color::White);
-	this->shape.setOrigin(this->wormRadius, this->wormRadius);
+	this->sprite.setPosition(this->posX, this->posY);
+	//this->sprite.setRadius(wormRadius);
+	//this->sprite.setFillColor(sf::Color::White);
+	
+	if (!texture.loadFromFile(textureImagePath))
+		return;
+
+	this->sprite.setTexture(texture);
+	this->sprite.setOrigin(this->wormRadius, this->wormRadius);
+	this->isMoving = false;
 
 
 	if (!this->font.loadFromFile(fontPath))
@@ -19,7 +25,7 @@ Worm::Worm(float width, float height) : windowWidth(width), windowHeight(height)
 	debugTxt.setFillColor(sf::Color::White);
 	debugTxt.setCharacterSize(12);
 	debugTxt.setPosition(font.getLineSpacing(debugTxt.getCharacterSize()), font.getLineSpacing(debugTxt.getCharacterSize() * 2));
-	debugTxt.setString("Ball X:" + std::to_string(getWormX()) + " Y:" + std::to_string(getWormY()));
+	debugTxt.setString("Worm X:" + std::to_string(getWormX()) + " Y:" + std::to_string(getWormY()));
 }
 
 Worm::~Worm()
@@ -29,32 +35,69 @@ Worm::~Worm()
 
 void Worm::update()
 {
-	this->shape.move(velocity);
-	this->posX = this->shape.getPosition().x;
-	this->posY = this->shape.getPosition().y;
+	if(isMoving)
+		this->sprite.move(velocity);
+	this->posX = this->sprite.getPosition().x;
+	this->posY = this->sprite.getPosition().y;
 
-	if (this->left() < 0)
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
 	{
-		this->velocity.x = wormVelocity;
+		moveUp();
+		this->sprite.move(velocity);
+		std::cout << "UP\n";
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		moveDown();
+		this->sprite.move(velocity);
+		std::cout << "DOWN\n";
+	}
+	else
+	{
+		stopMove();
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		moveLeft();
+		this->sprite.move(velocity);
+		std::cout << "LEFT\n";
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		moveRight();
+		this->sprite.move(velocity);
+		std::cout << "RIGHT\n";
+	}
+	else
+	{
+		stopMove();
+	}
+	if (this->left() < 5)
+	{
+		stopMove();
+		std::cout << "LEFT BORDER\n";
 	}
 	else if (this->right() > this->windowWidth)
 	{
-		this->velocity.x = -wormVelocity;
+		stopMove();
 	}
 
 	if (this->top() < 0)
 	{
-		this->velocity.y = wormVelocity;
+		stopMove();
 	}
 	else if (this->bottom() > this->windowHeight)
 	{
-		this->velocity.y = -wormVelocity;
+		stopMove();
 	}
 }
 
+
+
 void Worm::draw(sf::RenderTarget & target, sf::RenderStates states) const
 {
-	target.draw(this->shape, states);
+	target.draw(this->sprite, states);
 
 }
 
@@ -83,6 +126,39 @@ float Worm::getWormX() const
 float Worm::getWormY() const
 {
 	return this->posY;
+}
+
+void Worm::stopMove()
+{
+	this->isMoving = false;
+}
+
+void Worm::moveUp()
+{
+	this->isMoving = true;
+	this->velocity.y = -baseVelocity;
+	this->velocity.x = 0;
+}
+
+void Worm::moveDown()
+{
+	this->isMoving = true;
+	this->velocity.y = baseVelocity;
+	this->velocity.x = 0;
+}
+
+void Worm::moveLeft()
+{
+	this->isMoving = true;
+	this->velocity.y = 0;
+	this->velocity.x = -baseVelocity;
+}
+
+void Worm::moveRight()
+{
+	this->isMoving = true;
+	this->velocity.y = 0;
+	this->velocity.x = baseVelocity;
 }
 
 sf::Text Worm::getDebugTxt()
--- Worms 2D/Worm.h
+++ Worms 2D/Worm.h
@@ -13,20 +13,31 @@ public:
 	float bottom() const;
 	float getWormX() const;
 	float getWormY() const;
+	void stopMove();
+	void moveUp();
+	void moveDown();
+	void moveLeft();
+	void moveRight();
+
 	sf::Text getDebugTxt();
 
 protected:
 	void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
 private:
-	sf::CircleShape shape;
+	//sf::CircleShape sprite;
+	sf::Sprite sprite;
+	sf::Texture texture;
 	sf::Font font;
 	std::string fontPath = "resources/fonts/sansation.ttf";
+	std::string textureImagePath = "resources/images/worm.png";
 	sf::Text debugTxt;
 	const float wormRadius{ 10.0f };
-	const float wormVelocity{ 5.0f };
+	const float wormVelocity{ 0.0f };
+	const float baseVelocity{ 5.0f };
 	sf::Vector2f velocity{ wormVelocity,wormVelocity };
 	float posX;
 	float posY;
 	float windowHeight;
 	float windowWidth;
+	bool isMoving;
 };
