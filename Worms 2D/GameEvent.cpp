b582f0f9a59082a92c8b169ff9b5b49ae9a2b3e7
Cherro <kaliszewski.dominik@gmail.com>
2018-10-08 20:35:51 +0200

    Add game event class

---
+++ Worms 2D/GameEvent.cpp
@@ -0,0 +1,27 @@
+﻿#include "GameEvent.h"
+
+
+
+
+GameEvent::GameEvent(GameWindow window)
+{
+	this->window = window.GetInstance();
+}
+
+GameEvent::~GameEvent()
+{
+}
+
+sf::Event GameEvent::GetInstance()
+{
+	return this->event;
+}
+
+void GameEvent::handleEvents()
+{
+	while (this->window->pollEvent(this->event))
+	{
+		if (this->event.type == sf::Event::Closed)
+			this->window->close();
+	}
+}
